9e51c511eec56ddf5efa0e86c9fdba8b8d04e1bb
MP <pakyoon05@gmail.com>
2025-06-13 20:07:18 -0500

    Added OunceConversion Methods and User Input

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,7 +1,17 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class App {
   public static void main(String[] args) {
-    System.out.println("You can create a driver program for the converter here!");
+    Scanner input = new Scanner(System.in);
+    System.out.println("Welcome to the Ounce Conversion Program!");
+    System.out.println("\nHow many ounces do you have? ");
+    int ounces = input.nextInt();
+    input.close();
+    Converter converter = new Converter(); 
+    System.out.println(converter.toPoundsAndOunces(ounces));
+    System.out.println(converter.toPounds(ounces));
+    System.out.println("\nThank you for using the OCP!");
   }
 }
--- app/src/main/java/org/example/Converter.java
+++ app/src/main/java/org/example/Converter.java
@@ -1,5 +1,13 @@
 package org.example;
 
 public class Converter {
-  // Your code goes here
+  public String toPounds(int ounces) {
+    double pounds = ounces / 16.0;//finds pounds from ounces
+    return String.format("%.4f %s", pounds, (pounds == 1.0 ? "lb" : "lbs"));//condition ? : if true : if false
+  }// returns pounds with 4 decimal places and either lb or lbs
+  public String toPoundsAndOunces(int ounces) {//finds pounds and makes remainder ounces
+    int pounds = ounces / 16;// truncates pounds
+    int remainingOunces = ounces % 16;//modulus for ounces
+    return String.format("%d %s %d oz", pounds, (pounds == 1.0 ? "lb" : "lbs"), remainingOunces);
+  }
 }
